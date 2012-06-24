8474b64627708067ad37056c448877dbe93cfac8
James Curran <dzejms@gmail.com>
2012-06-23 22:00:52 -0400

    Added some more steps
    Got to the point where I need a value for the diaper text box.  Think
    I'll fake it for now.

--- BabyTime.Specifications/Steps/MainLoggers.cs
+++ BabyTime.Specifications/Steps/MainLoggers.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using NUnit.Framework;
 using TechTalk.SpecFlow;
 using WatiN.Core;
 
@@ -11,30 +12,53 @@ namespace BabyTime.Specifications.Steps
     [Binding]
     public class MainLoggers
     {
+        private Browser browser;
+
+        [BeforeScenario()]
+        private void SetUp()
+        {
+            browser = new IE();
+        }
+
         [Given(@"I am on the main page")]
         public void GivenIAmOnTheMainPage()
         {
-            Browser browser = new IE();
             browser.GoTo("http://localhost:61571/");
-            browser.FindText(new Regex(@"BABYTI.ME"));
+            string headerText = browser.FindText(new Regex(@"BABYTI.ME"));
+            Assert.AreEqual("BABYTI.ME", headerText);
         }
 
         [When(@"I press the Diaper button")]
         public void WhenIPressTheDiaperButton()
         {
-            ScenarioContext.Current.Pending();
+            const string buttonName = "diaperButton";
+            Button diaperButton = browser.Button(buttonName);
+            if (!diaperButton.Exists)
+                Assert.Fail("Missing {0} button", buttonName);
+            diaperButton.Click();
         }
 
         [Then(@"the diaper clock resets")]
         public void ThenTheDiaperClockResets()
         {
-            ScenarioContext.Current.Pending();
+            string diaperTextValue = browser.TextField("diaperTimerTextBox").Value;
+            if (String.IsNullOrEmpty(diaperTextValue))
+                Assert.Fail("Diaper Timer text box was empty!");
+            TimeSpan span = TimeSpan.Parse(diaperTextValue);
+            Assert.True(span.Seconds > 0 && span.Seconds < 5);
+
         }
 
         [Then(@"a diaper event is logged on the back end\.")]
         public void ThenADiaperEventIsLoggedOnTheBackEnd_()
         {
             ScenarioContext.Current.Pending();
+        }
+
+        [AfterScenario()]
+        public void TearDown()
+        {
+            browser.Close();
         }
 
     }
