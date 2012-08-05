be2c3a1dc4f4189c3509d37477819767b63fa966
James Curran <Dzejms@gmail.com>
2012-08-05 00:50:57 -0400

    Meh
    Not sure I want to keep going forward with knockout.  This is getting
    weird.

--- BabyTime.WebUI/Scripts/Main.js
+++ BabyTime.WebUI/Scripts/Main.js
@@ -1,4 +1,7 @@
-﻿$(function() {
+﻿/// <reference path="knockout-2.1.0.debug.js" />
+
+
+$(function() {
     var data = [
         { Id: 1, Name: "Diaper"},
         { Id: 2, Name: "Food"},
@@ -10,13 +13,24 @@
         // data
         timers: ko.observableArray(data),
         timerToAdd: ko.observable(""),
-        
+        selectedTimer: ko.observable(null),
+
+
         // Behavior
         addTimer: function () {
             this.timers.push({ Name: this.timerToAdd() });
             this.timerToAdd("");
+        },
+        selectTimer: function () {
+            console.log("inside selectTimer");
+            viewModel.selectedTimer(this);
         }
     };
+
+    $(document).on("click", ".timer-delete", function() {
+        var itemToRemove = ko.dataFor(this);
+        viewModel.timers.remove(itemToRemove);
+    });
 
     ko.applyBindings(viewModel);
 });
